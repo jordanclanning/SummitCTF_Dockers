192914fdf41ebb093b282750f0cbe2790244123b
Jordan Lanning <jordan.c.lanning@gmail.com>
2026-04-17 16:39:30 -0400

    Update init.sql

--- Web/Challenge5Lann/init.sql
+++ Web/Challenge5Lann/init.sql
@@ -1,10 +1,7 @@
-CREATE DATABASE IF NOT EXISTS Summit;
-USE Summit;
-
-CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50),
-    password VARCHAR(50)
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT,
+    password TEXT
 );
 
 INSERT INTO users (username, password) VALUES ('admin', 'securepass123');
